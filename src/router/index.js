743b24cfb184a966f2f2552380aa1cf6fd337e62
Sam Sharff <samanthaleec@gmail.com>
2022-04-11 13:56:52 -0400

    adds signup page; successfully tests

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import TestView from "../views/TestView.vue";
+import SignUp from "../views/SignUp.vue";
 
 const routes = [
   {
@@ -21,6 +22,11 @@ const routes = [
     path: "/test",
     name: "test",
     component: TestView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp,
   },
 ];
 
